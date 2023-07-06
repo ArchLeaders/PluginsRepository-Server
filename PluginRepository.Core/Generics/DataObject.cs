4b70189ff2acea9940d29f27b45b288f062d9126
ArchLeaders <archparliament@gmail.com>
2023-07-06 10:10:48 -0700

    Create generic data object

---
+++ PluginRepository.Core/Generics/DataObject.cs
@@ -0,0 +1,9 @@
+﻿namespace PluginRepository.Core.Generics;
+
+public partial class DataObject : ObservableObject
+{
+    [ObservableProperty]
+    [property: BsonId]
+    [property: BsonRepresentation(BsonType.ObjectId)]
+    private string _id = string.Empty;
+}
