5ee69cc523e932f6e3c29e594497e8654b922cdc
ArchLeaders <archparliament@gmail.com>
2023-07-06 10:11:01 -0700

    Create MongoDataProvider

---
+++ PluginRepository.Core/DataAccess/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace PluginRepository.Core.DataAccess;
+public interface IDataProvider<T> where T : DataObject
+{
+    Task Add(T item);
+    Task<T> Get(Expression<Func<T, bool>> filter);
+    Task<List<T>> GetAll();
+    Task Update(T item);
+}
---
+++ PluginRepository.Core/DataAccess/MongoDataProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+
+namespace PluginRepository.Core.DataAccess;
+
+public class MongoDataProvider<T> : IDataProvider<T> where T : DataObject
+{
+    protected readonly IMongoCollection<T> _collection;
+
+    public MongoDataProvider(IDatabaseConnection connection)
+    {
+        _collection = connection.GetCollection<T>();
+    }
+
+    public async Task<List<T>> GetAll()
+    {
+        var result = await _collection.FindAsync(_ => true);
+        return await result.ToListAsync();
+    }
+
+    public async Task<List<T>> GetAll(Expression<Func<T, bool>> filter)
+    {
+        var result = await _collection.FindAsync(filter);
+        return await result.ToListAsync();
+    }
+
+    public async Task<T> Get(Expression<Func<T, bool>> filter)
+    {
+        var result = await _collection.FindAsync(filter);
+        return await result.FirstOrDefaultAsync();
+    }
+
+    public async Task Add(T item)
+    {
+        await _collection.InsertOneAsync(item);
+    }
+
+    public async Task Update(T item)
+    {
+        var filter = Builders<T>.Filter.Eq("Id", item.Id);
+        await _collection.ReplaceOneAsync(filter, item, new ReplaceOptions {
+            IsUpsert = true
+        });
+    }
+}
