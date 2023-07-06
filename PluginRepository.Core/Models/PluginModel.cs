973de73e0636aa9f8525b1d60cc9e7d586f45f16
ArchLeaders <archparliament@gmail.com>
2023-07-06 10:11:22 -0700

    Create database models

---
+++ PluginRepository.Core/Models/PluginModel.cs
@@ -0,0 +1,65 @@
+﻿using PluginRepository.Core.Generics;
+
+namespace PluginRepository.Core.Models;
+
+public enum PluginType
+{
+    CLI, GUI
+}
+
+public enum RequestStatus
+{
+    /// <summary>
+    /// The request has been approved (public)
+    /// </summary>
+    Approved,
+
+    /// <summary>
+    /// The request has been revoked (private)
+    /// </summary>
+    Revoked,
+    
+    /// <summary>
+    /// The request is in the process of review
+    /// </summary>
+    Reviewing,
+
+    /// <summary>
+    /// The request is pending approval for review
+    /// </summary>
+    Pending,
+}
+
+public partial class PluginModel : DataObject
+{
+    [ObservableProperty]
+    private UserDisplayModel _author;
+
+    [ObservableProperty]
+    private string _name = string.Empty;
+
+    [ObservableProperty]
+    private string _description = string.Empty;
+
+    [ObservableProperty]
+    private PluginType _type = PluginType.CLI;
+
+    [ObservableProperty]
+    private UserDisplayModel? _reviewer;
+
+    [ObservableProperty]
+    private List<CommentModel> _comments = new();
+
+    [ObservableProperty]
+    private DateTime _createdAt = DateTime.UtcNow;
+
+    [ObservableProperty]
+    private RequestStatus _status = RequestStatus.Pending;
+
+    public PluginModel(UserModel author, string name, string description)
+    {
+        _author = new(author);
+        _name = name;
+        _description = description;
+    }
+}
---
+++ PluginRepository.Core/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿namespace PluginRepository.Core.Models;
+
+public partial class UserModel : DataObject
+{
+    [ObservableProperty]
+    private string _objectIdentifier = string.Empty;
+
+    [ObservableProperty]
+    private string _name = string.Empty;
+
+    [ObservableProperty]
+    private string _displayName = string.Empty;
+
+    [ObservableProperty]
+    private string _email = string.Empty;
+
+    [ObservableProperty]
+    private string? _phone;
+
+    [ObservableProperty]
+    private List<PluginDisplayModel> _authoredPlugins = new();
+
+    partial void OnDisplayNameChanged(string? oldValue, string newValue)
+    {
+        UpdateAuthorReferenced(AuthoredPlugins, newValue);
+    }
+
+    /// <summary>
+    /// Updates the referenced <see cref="AuthoredPlugins"/> if the <see cref="DisplayName"/> is changed
+    /// </summary>
+    /// <returns></returns>
+    public static void UpdateAuthorReferenced(List<PluginDisplayModel> referenced, string displayName)
+    {
+        // throw new NotImplementedException();
+    }
+}
